34d4c9c891c2874c7326d106995ef869810fedb0
LuchoQQ <lucianosanchez125@hotmail.com>
2022-11-24 20:26:04 -0300

    spotlightr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,37 @@
-import { Flex } from '@chakra-ui/react'
-import type { NextPage } from 'next'
+import { Flex } from "@chakra-ui/react";
+import type { NextPage } from "next";
 
+// "position:relative; width:1px; min-width:100%;padding-bottom:56.25%;"
 const Home: NextPage = () => {
-  return (
-    <>
-      <Flex w='100vw' h='100vw'>
-        
-      </Flex>
-    </>
-  )
-}
+    return (
+        <>
+            <Flex w="100vw" h="100vw">
+                <div
+                    style={{
+                        position: "relative",
+                        width: "1px",
+                        minWidth: "100%",
+                        paddingBottom: "56,25%",
+                    }}
+                >
+                    <iframe
+                        allow="autoplay"
+                        className="spotlightr"
+                        src="https://videos.cdn.spotlightr.com/watch/MTMyMjc1NQ==?fallback=true"
+                        scrolling="no"
+                        name="videoPlayer"
+                        style={{
+                            width: "720px",
+                            height: "480px",
+                        }}
+                        allowFullScreen={true}
+                    >
+                        {" "}
+                    </iframe>
+                </div>
+            </Flex>
+        </>
+    );
+};
 
-export default Home
+export default Home;
