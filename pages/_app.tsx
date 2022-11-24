6bcd3741427e54a55d78169a27a45ac7aaf773b3
LuchoQQ <lucianosanchez125@hotmail.com>
2022-11-24 16:47:12 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({
+    colors: {
+      primary: '',
+      secondary: '',
+      tertiary: '',
+      quaternary: '',
+    },
+    fonts: {
+      primary: '',
+      secondary: '',
+    }
+  })
+
+
+    return (
+        <>
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
