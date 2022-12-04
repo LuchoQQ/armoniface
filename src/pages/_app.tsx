0754f9cc0e25d92a9c3e8bb7eb037cda5b0a75e1
LuchoQQ <lucianosanchez125@hotmail.com>
2022-12-04 13:49:46 -0300

    Head Title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,18 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import theme from "../../theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
     return (
         <>
+            <Head>
+                <title>Armoniface</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+            </Head>
             <SessionProvider session={pageProps.session}>
                 <ChakraProvider theme={theme}>
                     <Component {...pageProps} />
