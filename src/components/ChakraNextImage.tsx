f8c27c88de692f4d6b83065fdf3641e9cc627987
luchoqq <lucianosanchez125@hotmail.com>
2023-01-09 19:01:13 -0300

    hero and about

--- src/components/ChakraNextImage.tsx
+++ src/components/ChakraNextImage.tsx
@@ -1,12 +1,12 @@
 import {Box} from "@chakra-ui/react";
 import Image from "next/image";
 
-const ChakraNextImage: any = ({src, alt, priority, ...props}: any) => {
+const ChakraNextImage: any = ({src, alt, priority, fit,...props}: any) => {
   const isPriority = priority ? true : false;
 
   return (
     <Box height="100%" position="relative" width="100%" {...props}>
-      <Image alt={alt} priority={true} fill style={{ objectFit: 'fill' }} quality="100" src={src} sizes="100%" />
+      <Image alt={alt} priority={true} fill style={{ objectFit: fit }} quality="100" src={src} sizes="100%" />
     </Box>
   );
 };
