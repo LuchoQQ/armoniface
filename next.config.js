b786f6a75f5acf47fe8177a3bf9d4b3f71cd8bae
LuchoQQ <lucianosanchez125@hotmail.com>
2023-03-07 03:43:26 -0300

    delete cors

--- next.config.js
+++ next.config.js
@@ -15,33 +15,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 }; */
 
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "http://armoniface.com/:path*",
-            },
-        ];
-    },
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-                    },
-                ],
-            },
-        ];
-    },
+
     reactStrictMode: true,
     swcMinify: true,
     images: {
