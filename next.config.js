bf6d27872b28984782f149a7234954362baaabaa
LuchoQQ <lucianosanchez125@hotmail.com>
2023-03-06 21:01:23 -0300

    cors updated

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/:path*",
+                source: "*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
